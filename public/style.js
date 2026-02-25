52af6e1cdf67ef90d1027d8b6c05828f2e43098e
Wartburg101 <wartburgerik@outlook.com>
2026-02-25 16:47:05 +0100

    Login Screen Partly Finished

--- public/style.js
+++ public/style.js
@@ -9,3 +9,17 @@ function toggleAreaButton() {
   sortButtonArea.classList.add("active");
   sortButtonAlphabet.classList.remove("active");
 }
+
+let signInNavButton = document.getElementById("signInNavButton");
+let crossDownLogInButton = document.getElementById("crossDownLogInButton");
+let loginScreen = document.getElementById("login-screen");
+
+function toggleLoginButton(){
+  
+  if (loginScreen.classList.contains("activeLogin")) {
+    loginScreen.classList.remove("activeLogin");
+  } else {
+    loginScreen.classList.add("activeLogin");
+  }
+
+}
